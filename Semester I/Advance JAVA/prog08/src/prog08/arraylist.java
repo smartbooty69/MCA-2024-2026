60383750441c2cb8b8761c6c00fe6f8bfa19f534
Clancy Mendonca <clancymendonca@gmail.com>
2024-08-20 15:54:37 +0530

    20-08-2024

--- Semester I/Advance JAVA/prog08/src/prog08/arraylist.java
+++ Semester I/Advance JAVA/prog08/src/prog08/arraylist.java
@@ -1,24 +1,62 @@
-package prog08;
+class Course {
+    private String courseName;
+    private String courseId;
+    private String courseCategory;
 
-import java.util.ArrayList;
+    public Course(String courseName, String courseId, String courseCategory) {
+        this.courseName = courseName;
+        this.courseId = courseId;
+        this.courseCategory = courseCategory;
+    }
 
-public class arraylist {
-	public static void main(String[] args) {
-        ArrayList<String> students = new ArrayList<>();
+    public String getCourseName() {
+        return courseName;
+    }
 
-        students.add("Clancy");
-        students.add("Vernon");
-        students.add("Nevil");
-        students.add("Glen");
-        
-        System.out.println(students.get(1));
-        System.out.println(students.isEmpty());
+    public String getCourseId() {
+        return courseId;
+    }
 
-        students.set(1, "Anson");
-        students.remove(2);
-    
-        for( String student: students) {
-            System.out.println(student);
-        }
+    public String getCourseCategory() {
+        return courseCategory;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setCourseCategory(String courseCategory) {
+        this.courseCategory = courseCategory;
+    }
+
+    public void retrieveCourseFromDatabase(String courseName, String courseId, String courseCategory) {
+        this.setCourseName(courseName);
+        this.setCourseId(courseId);
+        this.setCourseCategory(courseCategory);
+    }
+
+    public void printCourseDetails() {
+        System.out.println("Course Name: " + getCourseName());
+        System.out.println("Course ID: " + getCourseId());
+        System.out.println("Course Category: " + getCourseCategory());
+    }
+
+    public void updateCourseName() {
+        setCourseName("Computing in Java");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Course course = new Course("Java Basics", "CS101", "Programming");
+        course.retrieveCourseFromDatabase("Java Basics", "CS101", "Programming");
+        course.printCourseDetails();
+        course.updateCourseName();
+        System.out.println("\nUpdated Course Details:");
+        course.printCourseDetails();
     }
 }
