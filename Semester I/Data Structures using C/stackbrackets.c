af91ed4bbf2fa6b768fb2fc04c4184d3fd99505e
Clancy Mendonca <clancymendonca@gmail.com>
2024-09-25 10:00:23 +0530

    Update stackbrackets.c

--- Semester I/Data Structures using C/stackbrackets.c
+++ Semester I/Data Structures using C/stackbrackets.c
@@ -1,213 +1,97 @@
-/*---------------------------------------------------------------
-   STACKbracets.C 
------------------------------------------------------------------
-   Clancy 
-   23-09-24
-----------------------------------------------------------------*/
+/*------------------------------------------------------------------
+stackbrackets.c
+Program to align brackets using stack.
 
-#include <stdio.h>
+Clancy Mendonca
+25-09-24
+-----------------------------------------------------------------*/
 
-#define SIZE 100  
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define size 100 
 
 typedef struct StackType {
-    double arr[SIZE];
-    int top;
+    char A[size];
+    int Top;
 } STACK;
-
-void InitStack();
-void Push(double num);
-double Pop();
-int StackEmpty();
-void Display();
-
 STACK S;
-
-int main() {
-    int N;
-    double F1, F2, F3;
-    int i = 1;  
-
-    InitStack(); 
-
-    
-    F1 = 0;
-    F2 = 1;
-
-    Push(F1); 
-    Push(F2); 
-
-    
-    printf("Enter the number of Fibonacci terms to display:\n");
-    scanf("%d", &N);
-
-    
-    if (N <= 0) {
-        printf("Please enter a positive integer.\n");
-        return 0;
-    } else if (N == 1) {
-        printf("Fibonacci Series: 0\n");
-        return 0;
-    } else if (N == 2) {
-        printf("Fibonacci Series: 0, 1\n");
-        return 0;
-    }
-
-    
-    while (i < N) {
-        F1 = Pop(); 
-        F2 = Pop(); 
-        F3 = F1 + F2; 
-
-        Push(F2); 
-        Push(F1); 
-        Push(F3); 
-
-        i++; 
-    }
-
-    
-    printf("Fibonacci Series: ");
-    Display();
-
-    return 0;
-}
-
-
-void InitStack() {
-    S.top = -1; 
-}
-
-
-void Push(double num) {
-    if (S.top == SIZE - 1) {
-        printf("Stack Overflow!\n");
-        return;
-    } else {
-        S.top++;
-        S.arr[S.top] = num;
-    }
-}
-
-
-double Pop() {
-    if (S.top == -1) { 
-        printf("Stack Underflow!\n");
-        return -1;
-    } else {
-        double num = S.arr[S.top];
-        S.top--;
-        return num;
-    }
-}
-
-
-int StackEmpty() {
-    return (S.top == -1); 
-}
-
-
-void Display() {
-    for (int i = S.top; i >= 0; i--) { 
-        printf("%.0lf", S.arr[i]);
-        if (i != 0) {
-            printf(", ");
-        }
-    }
-    printf("\n");
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#define SIZE 100  
-
-typedef struct StackType {
-    char arr[SIZE];
-    int top;
-} STACK;
 
 void InitStack();
 void Push(char ch);
 char Pop();
-int StackEmpty();
-int IsBalanced(const char *exp);
-
-STACK S;
+int IsStackEmpty();
+int IsMatchingPair(char open, char close);
 
 int main() {
-    char exp[SIZE];
+    char BracketStr[size];
+    int i;
+    char Ch;
+    
+    printf("Enter the string of brackets: ");
+    scanf("%s", BracketStr);
 
-    printf("Enter an expression with brackets:\n");
-    scanf("%s", exp);
+    InitStack();
 
-    if (IsBalanced(exp)) {
-        printf("The expression has balanced brackets.\n");
+    for(i = 0; i < strlen(BracketStr); i++) {
+        char Brkt = BracketStr[i];
+
+        if(Brkt == '(' || Brkt == '{' || Brkt == '[') {
+            Push(Brkt);
+        } 
+        else if(Brkt == ')' || Brkt == '}' || Brkt == ']') {
+            if(IsStackEmpty()) {
+                printf("Improper bracket alignment.\n");
+                return 0;  
+            }
+            Ch = Pop();  
+            if(!IsMatchingPair(Ch, Brkt)) {
+                printf("Improper bracket alignment.\n");
+                return 0;  
+            }
+        }
+    }
+
+    if(IsStackEmpty()) {
+        printf("Brackets are properly aligned.\n");
     } else {
-        printf("The expression has unbalanced brackets.\n");
+        printf("Improper alignment of brackets.\n");
     }
 
     return 0;
 }
 
-
 void InitStack() {
-    S.top = -1; 
+    S.Top = -1;  
 }
-
 
 void Push(char ch) {
-    if (S.top == SIZE - 1) {
-        printf("Stack Overflow!\n");
+    if(S.Top == size - 1) {
+        printf("The Stack is full\n");
         return;
-    } else {
-        S.top++;
-        S.arr[S.top] = ch;
     }
+    S.Top++;
+    S.A[S.Top] = ch;
 }
-
 
 char Pop() {
-    if (S.top == -1) { 
-        return '\0'; 
-    } else {
-        char ch = S.arr[S.top];
-        S.top--;
-        return ch;
+    if(S.Top == -1) {  
+        printf("Stack is empty\n");
+        return -1;  
     }
+    char ch = S.A[S.Top];
+    S.Top--;
+    return ch;
 }
 
-
-int StackEmpty() {
-    return (S.top == -1); 
+int IsStackEmpty() {
+    return S.Top == -1; 
 }
 
-
-int IsBalanced(const char *exp) {
-    InitStack(); 
-
-    for (int i = 0; exp[i] != '\0'; i++) {
-        char ch = exp[i];
-
-        
-        if (ch == '(' || ch == '{' || ch == '[') {
-            Push(ch);
-        }
-        
-        else if (ch == ')' || ch == '}' || ch == ']') {
-            
-            if (StackEmpty()) {
-                return 0; 
-            }
-            char top = Pop(); 
-            
-            if ((ch == ')' && top != '(') ||
-                (ch == '}' && top != '{') ||
-                (ch == ']' && top != '[')) {
-                return 0; 
-            }
-        }
-    }
-
-    
-    return StackEmpty() ? 1 : 0; 
+int IsMatchingPair(char open, char close) {
+    if(open == '(' && close == ')') return 1;
+    if(open == '{' && close == '}') return 1;
+    if(open == '[' && close == ']') return 1;
+    return 0;
 }
